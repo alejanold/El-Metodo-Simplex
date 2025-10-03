e158b5897fcfd1e0d526a646e69e49b87a8c1ace
José Alejandro Canales Ortiz <iamaleortiz@gmail.com>
2025-10-02 22:53:34 -0600

    Ya empeze jaja simplex.py
    mas vale tarde que nunca, en unos minutos proseguimos

--- simplex.py
+++ simplex.py
@@ -1,1 +1,13 @@
-print ("Hola Mundo")
+def imprimir_tabla(tabla, variables, basicas):
+  print("\nTabla actual:")
+  encabezado = "     "
+  for nombre in variables:
+    encabezado += f"{nombre:^10}"
+    print(encabezado)
+
+for i, fila in enumerate(tabla):
+  if i == 0:
+            nombre = "Z"
+        else:
+            nombre = basicas[i - 1]
+        fila_str = f"{nombre:<4}"
