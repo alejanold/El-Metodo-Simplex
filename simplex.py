d9856dc0864412d546f09a9857bc9b85747aefc0
José Alejandro Canales Ortiz <iamaleortiz@gmail.com>
2025-10-03 00:10:31 -0600

    Update simplex.py
    apenas voy con los pivoteees

--- simplex.py
+++ simplex.py
@@ -38,3 +38,43 @@ def simplex(coef_objetivo, restricciones, lados_derechos, maximizar=True):
             tabla[i + 1][j] = restricciones[i][j]
         tabla[i + 1][num_variables + i] = 1 
         tabla[i + 1][-1] = lados_derechos[i]
+
+# Para crear nombres en las variables
+    variables = []
+    for i in range(num_variables):
+        variables.append(f"x{i+1}")
+    for i in range(num_restricciones):
+        variables.append(f"s{i+1}")
+    variables.append("RHS")
+
+    basicas = [f"s{i+1}" for i in range(num_restricciones)]
+
+    imprimir_tabla(tabla, variables, basicas)
+
+iteracion = 1
+    while True:
+        # Aqui comienzo a buscar la  columna pivote (o en dado caso el no. más negativo en fila Z)
+        col_pivote = -1
+        minimo = 0
+        for j in range(columnas - 1):
+            if tabla[0][j] < minimo:
+                minimo = tabla[0][j]
+                col_pivote = j
+        if col_pivote == -1:
+            print("No hay más coeficientes negativos en Z. ¡Solución óptima encontrada!")
+            break
+
+      # Continuo buscando la fila pivote (o la de menor razón positiva)
+        fila_pivote = -1
+        menor_razon = float("inf")
+        for i in range(1, filas):
+            if tabla[i][col_pivote] > 0:
+                razon = tabla[i][-1] / tabla[i][col_pivote]
+                if razon < menor_razon:
+                    menor_razon = razon
+                    fila_pivote = i
+        if fila_pivote == -1:
+            print("Problema ilimitado.")
+            return
+        print(f"\nIteración {iteracion}: Columna pivote = {col_pivote+1}, Fila pivote = {fila_pivote+1}")
+
